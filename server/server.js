619a84715f5772bbb652e7238ab8bef66345319e
Nizam Ahamad <nizam.ahamad@example.com>
2026-09-10 20:42:32 +0530

    Fix nodemailer timeout

--- server/server.js
+++ server/server.js
@@ -236,14 +236,14 @@ app.post('/api/auth/forgot-password', async (req, res) => {
     // We create a test account every time since we didn't specify one
     let testAccount = await nodemailer.createTestAccount();
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, 
-      auth: {
-        user: testAccount.user, 
-        pass: testAccount.pass, 
-      },
-    });
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
     const mailOptions = {
       from: '"CloudGuard Support" <support@cloudguard.com>',
