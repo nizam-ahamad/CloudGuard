38bcb92f250d705ab80175a3fad13b3a78a2a61a
Nizam Ahamad <nizam.ahamad@example.com>
2026-09-16 14:26:50 +0530

    Fix S3 preview content type and update toast notification

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
