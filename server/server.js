406bd9ec00c96fdec4fc1ac178ad4eb70f749da0
Nizam Ahamad <tonizamahamad@gmail.com>
2026-09-21 18:58:51 +0530

    Remove S3 SDK requestChecksumCalculation to fix large video upload bottlenecks

--- server/server.js
+++ server/server.js
@@ -31,8 +31,7 @@ const s3 = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-  requestChecksumCalculation: 'WHEN_REQUIRED'
+  }
 });
 
 // File Metadata Schema
